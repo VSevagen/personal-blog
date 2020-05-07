b125500efb43b7dbf4a9e2548b765890505ef7ac
sevagen <sevagenv@gmail.com>
2020-05-07 11:02:31 +0530

    Load icons from local

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,8 @@
 import React from "react"
 import "../components/main.css"
+import Twitter from "../pages/blogs/images/twitter.png"
+import Linkedin from "../pages/blogs/images/linkedin.png"
+import Gitlab from "../pages/blogs/images/gitlab.png"
 
 export default function Footer() {
     return (
@@ -10,9 +13,9 @@ export default function Footer() {
             <div class="footer">
 
                 <div class="link-holder">
-                    <a href="https://gitlab.gnome.org/Sevagen" target="_blank" rel="noopener noreferrer"><img src="https://cdn.icon-icons.com/icons2/2248/PNG/512/gitlab_icon_137548.png" alt=""></img></a>
-                    <a href="https://twitter.com/SevagenV" target="_blank" rel="noopener noreferrer"><img src="https://cdn.icon-icons.com/icons2/1/PNG/256/social_Twitter_38.png" alt=""></img></a>
-                    <a href="https://www.linkedin.com/in/veerasamy-sevagen/" target="_blank" rel="noopener noreferrer"><img src="https://cdn.icon-icons.com/icons2/1/PNG/256/sociallinkedin_member_30.png" alt=""></img></a>
+                    <a href="https://gitlab.gnome.org/Sevagen" target="_blank" rel="noopener noreferrer"><img src={Gitlab} alt=""></img></a>
+                    <a href="https://twitter.com/SevagenV" target="_blank" rel="noopener noreferrer"><img src={Twitter} alt=""></img></a>
+                    <a href="https://www.linkedin.com/in/veerasamy-sevagen/" target="_blank" rel="noopener noreferrer"><img src={Linkedin} alt=""></img></a>
                 </div>
 
                 <div class="copyright text-center">Copyright © 2020 - Veerasamy S</div>
