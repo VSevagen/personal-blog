c32bd9666a11aec29e0f2a90e25420a71f2b5d80
sevagen <sevagenv@gmail.com>
2020-05-08 23:17:19 +0530

    Set up google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,12 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
+      }
+    }
+  ],
 }
